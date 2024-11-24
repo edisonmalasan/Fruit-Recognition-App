0a08f372cbe6ec2b91ad329ea8f366fc3db4fdaf
Renz C. Dadpaas <2244751@slu.edu.ph>
2024-11-24 09:44:48 +0800

    changed Thread.sleep freezes entire main UI to delay which only delays the display of next task

--- app/src/main/java/com/example/strawberryripenessdetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/strawberryripenessdetectionapp/MainActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        Thread.sleep(3000)
+        lifecycleScope.launch{
+            delay(3000)
+        }
         installSplashScreen()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
