e1e74edfd7c445b0aa8a00286df7a958363c6fb1
Renz Dadpaas <2244751@slu.edu.ph>
2024-12-08 22:05:26 +0800

    added greeting page

--- app/src/main/java/com/example/fruitrecognitionapp/GreetingActivity.kt
+++ app/src/main/java/com/example/fruitrecognitionapp/GreetingActivity.kt
@@ -1,11 +1,43 @@
 package com.example.fruitrecognitionapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class GreetingActivity : AppCompatActivity() {
+    private lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting)
+
+        database = FirebaseDatabase.getInstance().getReference("users")
+        val userInput : EditText = findViewById<EditText>(R.id.user_name)
+        val submitButton : Button = findViewById<Button>(R.id.get_started)
+
+        submitButton.setOnClickListener{
+            val userName = userInput.text.toString()
+            if (userName.isNotEmpty()){
+                val user = User(userInput.text.toString())
+                val userID = database.push().key
+                if (userID != null) {
+                    database.child(userID).setValue(user).addOnSuccessListener {
+                        Toast.makeText(this, "User added successfully!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }.addOnFailureListener{
+                        Toast.makeText(this, "Failed to add user!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else{
+                Toast.makeText(this, "Please enter a name!", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
